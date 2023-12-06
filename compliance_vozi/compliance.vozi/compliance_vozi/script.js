ae79d57b4c7997a6a592e5773340fbcde33bfcc2
AmruthaSAathreya <150677734+AmruthaSAathreya@users.noreply.github.com>
2023-12-06 23:17:40 +0530

    Amrutha's commit on compliance_vozi

---
+++ compliance_vozi/compliance.vozi/compliance_vozi/script.js
@@ -0,0 +1,17 @@
+function requestOTP() {
+    // Mock function to simulate OTP request
+    // In a real scenario, you should send a request to your server
+    alert("OTP sent to your phone number");
+    document.getElementById('otpSection').style.display = 'block';
+}
+
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    var phone = document.getElementById('phone').value;
+    var otp = document.getElementById('otp').value;
+
+    // Validate OTP here
+    // Send phone and OTP to your server for verification
+
+    alert("Login successful with phone: " + phone + " and OTP: " + otp);
+});
---
+++ compliance_vozi/mainpage.js
@@ -0,0 +1,7 @@
+document.getElementById('vehiclesButton').addEventListener('click', function() {
+    window.location.href = 'vehicles1.html'; // Redirect to vehicles page
+});
+
+document.getElementById('driversButton').addEventListener('click', function() {
+    window.location.href = 'driver1.html'; // Redirect to drivers page
+});
